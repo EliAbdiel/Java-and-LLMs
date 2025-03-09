cdecfc2f3b232ef7306f33b0911f00ece7c6b6d8
EliAbdiel <371.abdiel@gmail.com>
2025-03-08 20:09:05 -0500

    Create multiple metadata filter expression

---
+++ metadata-filters/_005-more-filter-expression/src/main/java/xyz/eliabdiel/model/Product.java
@@ -0,0 +1,25 @@
+package xyz.eliabdiel.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    private int id;
+
+    @JsonProperty("product_name")
+    private String productName;
+    private String description;
+    private String brand;
+    private double price;
+    private int year;
+    private String country;
+
+    @JsonProperty("isAvailable")
+    private boolean isAvailable;
+}
